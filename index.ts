6c6cdb179a55415cbc462ab8ef758f8539dc254a
dalviik <duglasapazaa@gmail.com>
2021-02-01 18:58:21 -0400

    Update text index

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import express from 'express';
 // rest of the code remains same
 const app = express();
 const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.get('/', (req, res) => res.send('This is a Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
